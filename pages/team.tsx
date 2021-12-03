1c4d14bb664ba33929daf328103d0b7d8728ae20
vorillaz <theodorevorillas@gmail.com>
2021-12-03 14:51:13 +0200

    init

---
+++ pages/team.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Team: NextPage = () => {
+  return <div className={styles.container}>marketing Team page</div>;
+};
+
+export default Team;
